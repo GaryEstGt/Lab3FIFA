c3df589385975869fbf107e5dcfe502b0795a6e9
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-03-10 22:10:57 -0600

    Add files via upload

---
+++ Lab3FIFA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3FIFA.Startup))]
+namespace Lab3FIFA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
